56484110d97897c392791e270058b5de0df07f2f
Mmy2000 <yahianada005@gmail.com>
2023-12-01 01:02:19 +0200

    creating register functionality part 2

--- project/settings.py
+++ project/settings.py
@@ -134,3 +134,8 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR,"media")
+
+from django.contrib.messages import constants as messages
+MESSAGE_TAGS = {
+    messages.ERROR: 'danger',
+}
