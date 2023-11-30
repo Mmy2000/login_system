fb3c63ecd30b54c462795eaf2a3878b97faa50db
Mmy2000 <yahianada005@gmail.com>
2023-11-30 23:08:59 +0200

    creating base page part 2

--- project/urls.py
+++ project/urls.py
@@ -18,10 +18,12 @@ from django.contrib import admin
 from django.urls import path , include
 from django.conf import settings
 from django.conf.urls.static import static
+from . import views
 
 urlpatterns = [
     path('accounts/', include('accounts.urls')),
     path('admin/', admin.site.urls),
+    path('', views.home, name='home'),
 ]
 urlpatterns +=  static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
