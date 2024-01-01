445994a928991c4e9b58a8db04874e05d1db7e35
Mmy2000 <yahianada005@gmail.com>
2024-01-01 21:10:54 +0200

    updating

--- accounts/profile.py
+++ accounts/profile.py
@@ -1,6 +1,9 @@
 from .models import Profile
 
 def myprofile(request):
-    profile = Profile.objects.get(user=request.user)
+    current_user = request.user
+    profile = None
+    if  current_user.is_authenticated:
+        profile = Profile.objects.get(user=current_user)
 
     return dict(profile=profile)
